91e7187710fe81e2e3fd6cef815b9b2e9a9c2ba2
contactteambodywise-coder <contactteambodywise@gmail.com>
2026-01-26 17:46:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Blue Universe homepage loaded.");
+// For now, we’ll just log the page loaded
+console.log("J&V's Blue Universe loaded");
+
+// Later, we can add interactive stars, products, or hover effects
