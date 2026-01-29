13243b9431baef3eb78f6e3afdc5df3cfa93caf5
contactteambodywise-coder <contactteambodywise@gmail.com>
2026-01-29 13:44:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /* ======================== */
-/*   BLUE UNIVERSE: STARS   */
+/*   BLUE UNIVERSE STARTER  */
 /* ======================== */
 
 /* ===== Canvas Setup ===== */
@@ -13,37 +13,15 @@ function resizeCanvas() {
 window.addEventListener("resize", resizeCanvas);
 resizeCanvas();
 
-/* ===== STAR DATA ===== */
-const stars = Array.from({ length: 300 }, () => ({
-  x: Math.random() * canvas.width,
-  y: Math.random() * canvas.height,
-  r: Math.random() * 1.5 + 0.5,   // radius
-  a: Math.random() * 0.8 + 0.2    // opacity
-}));
-
-/* ===== DRAW STARS ===== */
-function drawStars() {
-  stars.forEach(s => {
-    ctx.globalAlpha = s.a;
-    ctx.beginPath();
-    ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2);
-    ctx.fillStyle = "white";
-    ctx.fill();
-  });
-  ctx.globalAlpha = 1;
-}
-
-/* ===== ANIMATION LOOP ===== */
+/* ===== Blank Animation Loop ===== */
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Draw stars first
-  drawStars();
-
-  // Future components (worm, shooting stars, etc.) go here
+  // Everything you want to add goes here
+  // Example: stars, worm, shooting stars, nebulae, etc.
 
   requestAnimationFrame(draw);
 }
 
-/* ===== START ANIMATION ===== */
+/* ===== Start Animation ===== */
 draw();
